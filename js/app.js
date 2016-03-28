9924a4d51fcb6feace8c0daad6a1af3d24f54acb
vmanamino <vmanamino@gmail.com>
2016-03-28 04:03:05 +0000

    check input and modify page

--- js/app.js
+++ js/app.js
@@ -16,7 +16,9 @@ var word = {
     }
 };
 
-var words = ["cycle", "bicyle"];
+var numberAt = 1;
+
+var words = ["secret"];
 
 $(document).ready(function(){
     var secret = Object.create(word);
@@ -38,20 +40,48 @@ function quizzing(){
     for (var i = 0; i < words.length; i += 1) {
         var quiz = Object.create(word);
         quiz.entry = words[i];
-        $("#gess").append("<tr id=\"sequence\"></tr>")
+        // $("#gess").append("<tr id=\"sequence\"></tr>")
         $("#gess").append("<tr id=\"letters\"></tr>");
-        var arr = [];
-        arr = quiz.letters();
-        for (var i = 0; i < arr.length; i += 1) {
-            $("tr#letters").append("<td>" + arr[i] + "</td>");
-        }
-        var arr = [];
-        arr = quiz.indices();
+        var arrLetters = [];
+        arrLetters = quiz.letters();
+        var o = arrLetters.length - numberAt;
+        // for (var i = 0; i < arrLetters.length; i += 1) {
+        //     $("tr#letters").append("<td>" + arrLetters[i] + "</td>");
+        // }
+        var arrIndices = [];
+        arrIndices = quiz.indices();
         $("#gess").append("<tr id=\"code\"></tr>");
-        for (var i = 0; i < arr.length; i += 1) {
-            $("tr#code").append("<td>" + arr[i] + "</td>");
+        // for (var i = 0; i < arrIndices.length; i += 1) {
+        //     $("tr#code").append("<td>" + arrIndices[i] + "</td>");
+        // }
+        for (var j = 0; j < arrLetters.length / 2; j++){
+            if (j == 0) {
+                $("tr#letters").append("<td>" + arrLetters[j] + "</td>");
+                $("tr#code").append("<td>" + arrIndices[j] + "</td>");
+                for (var k = 0; k < o - numberAt; k++) {
+                    if (k == 0){
+                        $("tr#letters").append("<td id=\"letterIn\"></td>");
+                        $("#letterIn").append("<form id=\"myGess\"></form>");
+                        $("#myGess").prepend("<input type=\"text\">");
+                        $("#myGess").append("<button type=\"button\">X</button>");
+                        $("#myGess").on("click", "button", function(){
+                            var enput = $("#myGess input").val();
+                            checkIn(enput, quiz); 
+                        });
+                    }
+                    else {
+                        $("tr#letters").append("<td>-</td>");
+                        // $("tr#code").append("<td>-</td>");
+                    }
+                    $("tr#code").append("<td>-</td>");
+                }
+                $("tr#letters").append("<td>" + arrLetters[o] + "</td>");
+                $("tr#code").append("<td>" + arrIndices[o] + "</td>");
+            }
+            else {
+                // addRest();
+            }
         }
-        
     }
 }
 
@@ -59,4 +89,35 @@ function letterNumber(littera){
     var alphaomega = "abcdefghijklmnopqrstuvwxyz";
     var place = alphaomega.indexOf(littera);
     return place + 1;
+}
+
+function checkIn(enPut, quiz){
+    console.log("checkIn called");
+    var letters = quiz.letters();
+    if (!enPut){
+        alert("no input, try again");
+    }
+    else{
+        if (enPut == letters[numberAt]){
+            console.log("correct");
+            numberAt++;
+            console.log(numberAt);
+            addRest(quiz);
+        }
+        else {
+            alert("Try again");
+        }
+    }
+}
+
+function addRest(quiz){
+    var letters = quiz.letters();
+    var indices = quiz.indices();
+    var start = numberAt - 1;
+    console.log(start);
+    var end = letters.length - numberAt;
+    for (var l = 0; l < 1; l++){
+        $("td#letterIn").remove();
+        $("<td>" + letters[start] + "</td>").insertAfter("tr#letters>td:nth-child("+start+")");
+    }
 }

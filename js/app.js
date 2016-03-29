5a696a19ccc1ed4ea88cd9c16f04ee1f8a1539eb
vmanamino <vmanamino@gmail.com>
2016-03-29 04:34:39 +0000

    basic right-left processing

--- js/app.js
+++ js/app.js
@@ -58,23 +58,25 @@ function quizzing(){
             if (j == 0) {
                 $("tr#letters").append("<td>" + arrLetters[j] + "</td>");
                 $("tr#code").append("<td>" + arrIndices[j] + "</td>");
-                for (var k = 0; k < o - numberAt; k++) {
-                    if (k == 0){
-                        $("tr#letters").append("<td id=\"letterIn\"></td>");
-                        $("#letterIn").append("<form id=\"myGess\"></form>");
-                        $("#myGess").prepend("<input type=\"text\">");
-                        $("#myGess").append("<button type=\"button\">X</button>");
-                        $("#myGess").on("click", "button", function(){
-                            var enput = $("#myGess input").val();
-                            checkIn(enput, quiz); 
-                        });
-                    }
-                    else {
-                        $("tr#letters").append("<td>-</td>");
-                        // $("tr#code").append("<td>-</td>");
-                    }
-                    $("tr#code").append("<td>-</td>");
-                }
+                console.log(numberAt);
+                innerCircle(o, numberAt, quiz);
+                // for (var k = 0; k < o - numberAt; k++) {
+                //     if (k == 0){
+                //         $("tr#letters").append("<td id=\"letterIn\"></td>");
+                //         $("#letterIn").append("<form id=\"myGess\"></form>");
+                //         $("#myGess").prepend("<input type=\"text\">");
+                //         $("#myGess").append("<button type=\"button\">X</button>");
+                //         $("#myGess").on("click", "button", function(){
+                //             var enput = $("#myGess input").val();
+                //             checkIn(enput, quiz); 
+                //         });
+                //     }
+                //     else {
+                //         $("tr#letters").append("<td>-</td>");
+                //         // $("tr#code").append("<td>-</td>");
+                //     }
+                //     $("tr#code").append("<td>-</td>");
+                // }
                 $("tr#letters").append("<td>" + arrLetters[o] + "</td>");
                 $("tr#code").append("<td>" + arrIndices[o] + "</td>");
             }
@@ -115,9 +117,47 @@ function addRest(quiz){
     var indices = quiz.indices();
     var start = numberAt - 1;
     console.log(start);
+    console.log("number at"+numberAt);
     var end = letters.length - numberAt;
+    console.log(end);
     for (var l = 0; l < 1; l++){
         $("td#letterIn").remove();
         $("<td>" + letters[start] + "</td>").insertAfter("tr#letters>td:nth-child("+start+")");
+        $("tr#code>td:nth-child("+numberAt+")").remove();
+        $("<td>" + indices[start] + "</td>").insertAfter("tr#code>td:nth-child("+start+")");
+        
+        $("tr#letters>td:nth-child("+(end + 1)+")").remove();
+        $("<td>"+letters[end]+"</td>").insertBefore("tr#letters>td:nth-child("+(end + 1)+")");
+        $("tr#code>td:nth-child("+(end + 1)+")").remove();
+        $("<td>"+ indices[end] + "</td>").insertBefore("tr#code>td:nth-child("+(end+1)+")");
+    }
+    $("tr#letters>td:nth-child("+(end)+")").remove();
+    $("tr#code>td:nth-child("+(end)+")").remove();
+    $("tr#letters>td:nth-child("+(start + 2)+")").remove();
+    $("tr#code>td:nth-child("+(start + 2)+")").remove();
+    innerCircle(end, numberAt, quiz);
+}
+
+function innerCircle(o, numberAt, quiz){
+    var start = numberAt;
+    for (var k = 0; k < o - numberAt; k++) {
+        if (k == 0){
+            $("<td id=\"letterIn\"></td>").insertAfter("tr#letters>td:nth-child("+(start)+")");
+            // $("tr#letters").append("<td id=\"letterIn\"></td>");
+            $("#letterIn").append("<form id=\"myGess\"></form>");
+            $("#myGess").prepend("<input type=\"text\">");
+            $("#myGess").append("<button type=\"button\">X</button>");
+            $("#myGess").on("click", "button", function(){
+                var enput = $("#myGess input").val();
+                checkIn(enput, quiz); 
+            });
+        }
+        else {
+            // $("tr#letters").append("<td>-</td>");
+            $("<td>-</td>").insertAfter("td#letterIn");
+            // $("tr#code").append("<td>-</td>");
+        }
+        // $("tr#code").append("<td>-</td>");
+        $("<td>-</td>").insertAfter("tr#code>td:nth-child("+(start)+")");
     }
 }
